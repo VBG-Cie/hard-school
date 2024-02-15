45e99cd0d5519d7279b8565ecd5fa7a5200ebb6a
PlexiAndersson <williamandersson2000@gmail.com>
2024-02-15 13:27:27 +0100

    Background color

--- src/components/EliteForm.tsx
+++ src/components/EliteForm.tsx
@@ -2,7 +2,7 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 const EliteForm = () => {
   return (
-    <Box>
+    <Box bg="gray.200">
       <Heading>Elite form</Heading>
       <Text>Hej Magnus</Text>
     </Box>
