4a10ffb33dff4fef76873dafd262b0e932d227f1
PlexiAndersson <williamandersson2000@gmail.com>
2024-02-15 13:07:22 +0100

    Very important changes

--- src/components/EliteForm.tsx
+++ src/components/EliteForm.tsx
@@ -1,9 +1,10 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const EliteForm = () => {
   return (
     <Box>
       <Heading>Elite form</Heading>
+      <Text>Hej Magnus</Text>
     </Box>
   );
 };
